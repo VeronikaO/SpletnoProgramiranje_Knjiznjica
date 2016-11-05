7456e6122b8a7d9d7d68cb219dce7c9a04ef9cfc
veronikao <veronika.omahen@gmail.com>
2016-11-05 17:50:15 +0100

    html framework

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Categories</a></li>" +
+"<li><a href='index.html'>Order by: </a></li>" +
+"<li><a href='index.html'>Coming soon</a></li>" +
+"<li>Search bar</li>" +
+"</ul>";
