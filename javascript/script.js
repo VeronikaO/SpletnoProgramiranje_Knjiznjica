9a89a7ab9a3bdb3936e176ccdb3d163e3aeaa0d8
veronikao <veronika.omahen@gmail.com>
2016-11-20 19:55:04 +0100

    olepsala css

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,8 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Categories</a></li>" +
-"<li><a href='index.html'>Order by: </a></li>" +
+"<li><div class=\"dropdown\"><button class=\"dropbtn\">Categories</button><div class=\"dropdown-content\"><a href=\"#\">Fantasy</a><a href=\"#\">Sci-fi</a><a href=\"#\">Comedy</a></div></div></li>" +
+"<li><div class=\"dropdown\"><button class=\"dropbtn\">Order by:</button><div class=\"dropdown-content\"><a href=\"#\">Latest</a><a href=\"#\">Most popular</a><a href=\"#\">Best match</a></div></div></li>" +
 "<li><a href='index.html'>Coming soon</a></li>" +
-"<li>Search bar</li>" +
+"<li><input type=\"search\" placeholder=\"Search\"></input></li>" +
+"<li><button type=\"submit\">Search</button></li>"
 "</ul>";
