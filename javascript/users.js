c7f92bed1275bb057655096b9b7c5ce0561245be
veronikao <veronika.omahen@gmail.com>
2016-11-26 20:52:45 +0100

    razno

---
+++ javascript/users.js
@@ -0,0 +1,20 @@
+var admins = [
+    {
+      "username":"Oni",
+      "password":"passwd"
+    },
+];
+
+var users = [
+    {
+      "username":"book_luvr",
+      "password":"books<3",
+      "name":"Micka",
+      "surname":"Novak",
+      "address":"ulica st",
+      "city":"mesto",
+      "zip":"1000",
+      "phone":"031-031-031",
+      "email":"email@gmail.com"
+    },
+];
